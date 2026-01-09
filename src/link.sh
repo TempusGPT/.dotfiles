89068a84492e5c59007911c74661e62eebe960fd
TempusGPT <me@changyong.me>
2026-01-09 22:49:05 +0900

    feat: add oh-my-opencode.json configuration and symlink

--- src/link.sh
+++ src/link.sh
@@ -5,6 +5,7 @@ mkdir -p ~/Library/Application\ Support/com.mitchellh.ghostty
 
 # Symlink configuration files
 ln -snf ~/.dotfiles/config/.config/karabiner/assets/complex_modifications/rules.json ~/.config/karabiner/assets/complex_modifications/rules.json
+ln -snf ~/.dotfiles/config/.config/opencode/oh-my-opencode.json ~/.config/opencode/oh-my-opencode.json
 ln -snf ~/.dotfiles/config/.config/opencode/opencode.json ~/.config/opencode/opencode.json
 ln -snf ~/.dotfiles/config/Library/Application\ Support/com.mitchellh.ghostty/config ~/Library/Application\ Support/com.mitchellh.ghostty/config
 ln -snf ~/.dotfiles/config/.gitconfig ~/.gitconfig
