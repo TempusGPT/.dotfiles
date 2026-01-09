507f298f157e4494bf5781b8961b370290ffd8ef
TempusGPT <me@changyong.me>
2026-01-09 20:41:30 +0900

    Add .hushlogin to suppress last login message and rename oh-my-zsh.sh to shell.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname $0)"
 
 source src/homebrew.sh
-source src/oh-my-zsh.sh
+source src/shell.sh
 source src/config.sh
 source src/nodejs.sh
 
--- src/shell.sh
+++ src/shell.sh
@@ -1,2 +1,5 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Suppress "Last login" message
+touch ~/.hushlogin
